dd3a47e86e19c4f3fc4f04067bc17678bc5a73a5
DRAGOS-ANDREI POPESCU <andrei.popescu29@yahoo.com>
2023-01-14 21:47:26 +0200

    Incepere form adaugare adresa

---
+++ PWA.Shared/DTOs/CladireDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWA.Shared.DTOs
+{
+	public sealed class CladireDto
+	{
+		[Required]
+		public string Strada { get; set; }
+        [Required]
+        public string Numar { get; set; }
+
+        public string Bloc { get; set; }
+        public string Scara { get; set; }
+        [Required]
+		public string Oras { get; set; }
+		[Required]
+		public string Judet { get; set; }
+	}
+}
