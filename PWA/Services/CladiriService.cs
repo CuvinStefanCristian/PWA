24ef8d777740f5fafe155bf1fc1d93adc4d3f660
Dragos-Andrei Popescu <andrei.popescu@infofer.ro>
2023-01-18 15:44:05 +0200

    adaugare tabel( in lucru)

--- PWA/Services/CladiriService.cs
+++ PWA/Services/CladiriService.cs
@@ -17,5 +17,12 @@ namespace PWA.Services
         {
             return await _httpService.PostAsync<CladireDto>("cladiri/insert", cladiriDto);
         }
+
+        public async Task<CustomResponse<List<CladireDto>>> GetCladiriAsync()
+        {
+
+           return await _httpService.GetAsync<List<CladireDto>>("cladiri/get");
+            
+        }
     }
 }
